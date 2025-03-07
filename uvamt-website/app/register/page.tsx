6b521f4d8eedc3151f91fe9f650b37e36bf27f08
trangium <vincent.k.trang@gmail.com>
2025-03-07 18:34:19 -0500

    change fixed height to 2700

--- uvamt-website/app/register/page.tsx
+++ uvamt-website/app/register/page.tsx
@@ -1,5 +1,5 @@
 export default function Register() {
     return <div className = "w-full text-center">
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={2500} frameBorder={0} marginHeight={0} marginWidth={0} />
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={2700} frameBorder={0} marginHeight={0} marginWidth={0} />
     </div>
 }
