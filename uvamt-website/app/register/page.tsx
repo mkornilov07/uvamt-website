af39d2d52e96d888f807a970c2c72dbd995d3fef
trangium <vincent.k.trang@gmail.com>
2025-03-07 18:31:10 -0500

    go back to fixed height, it's simler

--- uvamt-website/app/register/page.tsx
+++ uvamt-website/app/register/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
 export default function Register() {
-    function adjustIframeHeight(event: React.SyntheticEvent<HTMLIFrameElement>) {
-        const iframe = event.currentTarget;
-        if (iframe.contentWindow && iframe.contentWindow.document.body) {
-            iframe.style.height = `${iframe.contentWindow.document.body.scrollHeight}px`;
-        }
-    }
-
-    return <div className="w-full text-center">
-        <iframe className="m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={typeof window !== "undefined" ? (1260000 / Math.min(600, window.innerWidth)) : 2100} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
+    return <div className = "w-full text-center">
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={2500} frameBorder={0} marginHeight={0} marginWidth={0} />
     </div>
 }
