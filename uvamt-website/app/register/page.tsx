03e5ee7fcf9f7cc9272bdc290bb15c7a8e80455e
trangium <vincent.k.trang@gmail.com>
2025-03-07 18:08:26 -0500

    auto resize registration iframe based on width

--- uvamt-website/app/register/page.tsx
+++ uvamt-website/app/register/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 export default function Register() {
-    return <div className = "w-full text-center">
-    <iframe className = "m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={9999} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
+    function adjustIframeHeight(event: React.SyntheticEvent<HTMLIFrameElement>) {
+        const iframe = event.currentTarget;
+        if (iframe.contentWindow && iframe.contentWindow.document.body) {
+            iframe.style.height = `${iframe.contentWindow.document.body.scrollHeight}px`;
+        }
+    }
+
+    return <div className="w-full text-center">
+        <iframe className="m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={1250000/Math.min(600, innerWidth)} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
     </div>
 }
