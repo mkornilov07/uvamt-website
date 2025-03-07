c6b8532bc109dc6a933cf5a713a4dce33162f116
trangium <vincent.k.trang@gmail.com>
2025-03-07 18:20:21 -0500

    added fallback in case window width can't be found

--- uvamt-website/app/register/page.tsx
+++ uvamt-website/app/register/page.tsx
@@ -9,6 +9,6 @@ export default function Register() {
     }
 
     return <div className="w-full text-center">
-        <iframe className="m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={1250000/Math.min(600, innerWidth)} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
+        <iframe className="m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={typeof window !== "undefined" ? (1260000 / Math.min(600, window.innerWidth)) : 2100} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
     </div>
 }
