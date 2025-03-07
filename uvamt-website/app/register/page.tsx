8c78594163a2ae15c615b3ecb363f40c37f53d37
Mikhail <mkornilov07@gmail.com>
2025-03-07 17:06:59 -0500

    made form wider

--- uvamt-website/app/register/page.tsx
+++ uvamt-website/app/register/page.tsx
@@ -1,5 +1,5 @@
 export default function Register() {
     return <div className = "text-center">
-    <iframe className = "m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="70%" height={2381} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
+    <iframe className = "m-auto" src="https://docs.google.com/forms/d/e/1FAIpQLSd3O9TIiDp9xYY81jq9Fh86agvUkTHri8VnhWV8TXtixMbugQ/viewform?embedded=true" width="100%" height={2381} frameBorder={0} marginHeight={0} marginWidth={0}>Loading…</iframe>
     </div>
 }
