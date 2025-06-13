6a418119c9a130c80e316e95a987a96515d69b47
Mikhail <mkornilov07@gmail.com>
2025-06-13 12:17:27 -0700

    update main page for 2026, registration not yet open

--- uvamt-website/app/page.tsx
+++ uvamt-website/app/page.tsx
@@ -6,37 +6,37 @@ export default function Home() {
       <link rel="icon" href="favicon.ico" sizes="any" />
 
       <header className="mb-8 text-center">
-        <h1 className="text-3xl font-bold mb-2">1st University Of Virginia Math Tournament (UVAMT)</h1>
-        <p className="text-xl">Saturday, April 19, 2025</p>
+        <h1 className="text-3xl font-bold mb-2">2nd University Of Virginia Math Tournament (UVAMT)</h1>
+        <p className="text-xl">March/April 2026</p>
       </header>
 
       <main>
         <section className="mb-8">
           <h2 className="text-2xl font-semibold mb-4">About</h2>
           <p>
-            The 1st University Of Virginia Math Tournament (UVAMT) will be held on <span className="font-medium">Saturday, April 19</span> at the University of Virginia. UVAMT brings together talented students from across the region to tackle fun and challenging math problems.
+            The 2st University Of Virginia Math Tournament (UVAMT) will be held Spring 2026 at the University of Virginia. UVAMT brings together talented students from across the region to tackle fun and challenging math problems.
           </p>
         </section>
 
         <section className="mb-8">
           <h2 className="text-2xl font-semibold mb-4">Register</h2>
           <p className="mb-4">
-            Registration is <span className="font-medium">open</span>! Teams may register using the following form.
+            Registration is <span className="font-medium">not yet open</span>. Stay tuned for updates!
           </p>
-          <div className="text-center">
+          {/* <div className="text-center">
             <Link href="/register" className="inline-block relative px-6 py-3 font-bold text-white rounded-lg transition duration-300 
                    bg-gradient-to-r from-blue-600 to-blue-500 
                    hover:from-blue-800 hover:to-blue-700">
               Register Now
             </Link>
-          </div>
+          </div> */}
         </section>
 
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold mb-4">Schedule Outline</h2>
+          <h2 className="text-2xl font-semibold mb-4">Schedule Outline (Approximate)</h2>
           <div className="grid grid-cols-12 gap-x-6 gap-y-2">
             <div className="col-span-3 sm:col-span-2 font-medium text-right">9:00am</div>
-            <div className="col-span-9 sm:col-span-10">Check-in, Setup</div>
+            <div className="col-span-9 sm:col-span-10">Check-in</div>
             
             <div className="col-span-3 sm:col-span-2 font-medium text-right">10:00am</div>
             <div className="col-span-9 sm:col-span-10">Opening Ceremony</div>
