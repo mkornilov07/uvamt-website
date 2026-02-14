9d4785e05730c12bee055ab8890e2de537624633
Brendan Malaugh <superpsych10@gmail.com>
2026-02-14 16:39:42 -0500

    organizers only for now on team page for merge to main

--- uvamt-website/data/team.ts
+++ uvamt-website/data/team.ts
@@ -6,10 +6,10 @@ export interface TeamMember {
 }
 
 export const team: TeamMember[] = [
-    { name: "Vincent Trang", role: "Organizer", imageSrc: "/team/vincent_trang.jpg", bio: "Test Bio. My name is Vincent." },
-    { name: "Mikhail Kornilov", role: "Organizer", imageSrc: "/team/mikhail_kornilov.jpg", bio: "Testing a long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long long bio." },
-    { name: "Emma Ylagan", role: "Outreach Coordinator", imageSrc: "/team/emma_ylagan.jpg" },
-    { name: "Isaac Assink", role: "Volunteer", imageSrc: "/team/isaac_assink.jpg" },
-    { name: "Utkarsh Goyal", role: "Volunteer", imageSrc: "/team/utkarsh_goyal.jpg" },
-    { name: "Brendan Malaugh", role: "Volunteer", imageSrc: "/team/brendan_malaugh.jpg" },
+    { name: "Vincent Trang", role: "Organizer", imageSrc: "/team/vincent_trang.jpg", bio: "" },
+    { name: "Mikhail Kornilov", role: "Organizer", imageSrc: "/team/mikhail_kornilov.jpg", bio: "" },
+    { name: "Utkarsh Goyal", role: "Organizer", imageSrc: "/team/utkarsh_goyal.jpg" },
+    //{ name: "Emma Ylagan", role: "Outreach Coordinator", imageSrc: "/team/emma_ylagan.jpg" },
+    //{ name: "Isaac Assink", role: "Volunteer", imageSrc: "/team/isaac_assink.jpg" },
+    //{ name: "Brendan Malaugh", role: "Volunteer", imageSrc: "/team/brendan_malaugh.jpg" },
 ];
