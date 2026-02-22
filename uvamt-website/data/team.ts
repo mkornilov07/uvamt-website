ff3da7889d222867c0f8ab7d0f5934384125e6df
Mikhail Kornilov <mikhail@virginia.edu>
2026-02-22 18:31:53 -0500

    team

--- uvamt-website/data/team.ts
+++ uvamt-website/data/team.ts
@@ -6,10 +6,11 @@ export interface TeamMember {
 }
 
 export const team: TeamMember[] = [
-    { name: "Vincent Trang", role: "Organizer", imageSrc: "/team/vincent_trang.jpg", bio: "" },
-    { name: "Mikhail Kornilov", role: "Organizer", imageSrc: "/team/mikhail_kornilov.jpg", bio: "" },
-    { name: "Utkarsh Goyal", role: "Organizer", imageSrc: "/team/utkarsh_goyal.jpg" },
-    //{ name: "Emma Ylagan", role: "Outreach Coordinator", imageSrc: "/team/emma_ylagan.jpg" },
-    //{ name: "Isaac Assink", role: "Volunteer", imageSrc: "/team/isaac_assink.jpg" },
-    //{ name: "Brendan Malaugh", role: "Volunteer", imageSrc: "/team/brendan_malaugh.jpg" },
+    { name: "Vincent Trang", role: "Coordinator", imageSrc: "/team/vincent_trang.jpg", bio: "" },
+    { name: "Mikhail Kornilov", role: "Coordinator", imageSrc: "/team/mikhail_kornilov.jpg", bio: "" },
+    { name: "Utkarsh Goyal", role: "Coordinator", imageSrc: "/team/utkarsh_goyal.jpg" , bio:""},
+    { name: "Emma Ylagan", role: "Social Media Manager", imageSrc: "/team/emma_ylagan.jpg" , bio:""},
+    { name: "Isaac Assink", role: "Volunteer", imageSrc: "/team/isaac_assink.jpg" , bio:""},
+    { name: "Brendan Malaugh", role: "Web Developer", imageSrc: "/team/brendan_malaugh.jpg" , bio:""},
+    { name: "Eric Zeng", role: "Artist", imageSrc: "/team/eric_zeng.jpg", bio: ""}
 ];
