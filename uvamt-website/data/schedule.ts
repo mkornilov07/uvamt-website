4f31feb5789b6a5b980129043bd9178dbcd9cda4
Mikhail Kornilov <mikhail@virginia.edu>
2026-02-22 21:56:20 -0500

    Schedule bring indiv before lunch

--- uvamt-website/data/schedule.ts
+++ uvamt-website/data/schedule.ts
@@ -4,13 +4,13 @@ export interface ScheduleItem {
 }
 
 export const schedule: ScheduleItem[] = [
-    { time: "9:00 AM", activity: "Check-In" },
+    { time: "9:00 AM", activity: "Check-In + Breakfast" },
     { time: "10:00 AM", activity: "Opening Ceremony" },
-    { time: "10:30 AM", activity: "Team Round" },
-    { time: "11:15 AM", activity: "Optimization Round" },
-    { time: "11:55 AM", activity: "Lunch" },
-    { time: "1:10 PM", activity: "Individual Round" },
-    { time: "2:20 PM", activity: "Game Round" },
+    { time: "10:30 AM", activity: "Individual Round" },
+    { time: "11:30 AM", activity: "Team Round" },
+    { time: "12:00 PM", activity: "Lunch" },
+    { time: "1:15 PM", activity: "Optimization Round" },
+    { time: "2:10 PM", activity: "Game Round" },
     { time: "3:00 PM", activity: "Awards, Closing Ceremony" },
 ];
 
