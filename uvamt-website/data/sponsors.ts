e2e0960f688b3c9c331e686e29b584e25c4d1134
Brendan Malaugh <superpsych10@gmail.com>
2026-02-24 12:53:34 -0500

    we -> they

--- uvamt-website/data/sponsors.ts
+++ uvamt-website/data/sponsors.ts
@@ -9,13 +9,13 @@ export const sponsors: Sponsor[] = [
         name: "Jane Street",
         logoSrc: "/sponsors/janestreet_logo.png",
         blurb:
-            "Jane Street is a quantitative trading firm and liquidity provider with a unique focus on technology and collaborative problem solving. We look for smart people with curious minds from any background."
+            "Jane Street is a quantitative trading firm and liquidity provider with a unique focus on technology and collaborative problem solving. They look for smart people with curious minds from any background."
     },
     {
         name: "Hudson River Trading",
         logoSrc: "/sponsors/hrt_logo.png",
         blurb:
-            "HRT is first and foremost a math and technology company. We are engineers and researchers working as one team to solve difficult problems, and trading millions of shares a day on the world’s financial markets."
+            "HRT is first and foremost a math and technology company. They are engineers and researchers working as one team to solve difficult problems, and trading millions of shares a day on the world’s financial markets."
     },
     {
         name: "SPARC",
