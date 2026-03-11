033bc1e51f8a0be5c696bd9195e12731f19a4ecb
Mikhail Kornilov <mikhail@virginia.edu>
2026-03-11 12:34:45 -0400

    add panel

--- uvamt-website/data/schedule.ts
+++ uvamt-website/data/schedule.ts
@@ -8,7 +8,7 @@ export const schedule: ScheduleItem[] = [
     { time: "10:00 AM", activity: "Opening Ceremony" },
     { time: "10:30 AM", activity: "Individual Round" },
     { time: "11:30 AM", activity: "Team Round" },
-    { time: "12:00 PM", activity: "Lunch" },
+    { time: "12:00 PM", activity: "Lunch + Panel" },
     { time: "1:15 PM", activity: "Optimization Round" },
     { time: "2:10 PM", activity: "Game Round" },
     { time: "3:00 PM", activity: "Awards, Closing Ceremony" },
